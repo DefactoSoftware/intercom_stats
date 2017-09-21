c4fd8429060c9d9ce66bcda96ce91688b0aa9aac
Blackjaguar18 <online_richard@hotmail.com>
2017-09-21 10:44:51 +0200

    include queries for retrieving data

--- lib/intercom_stats/repository/conversations.ex
+++ lib/intercom_stats/repository/conversations.ex
@@ -1,8 +1,45 @@
 defmodule IntercomStats.Repository.Conversations do
   alias IntercomStats.Intercom.Conversation
   alias IntercomStats.Repo
+  alias IntercomStats.Repository.Tags
+  alias IntercomStats.Repository.Segments
+  import Ecto.Query
 
   def list_all_conversations() do
     Repo.all(Conversation)
+  end
+
+  def list_conversations_by_segments(segments_list) do
+    query = from s in "segments",
+            join: c in "conversations", on: s.id == c.segment_id,
+            where: s.name in ^segments_list,
+            select: %Conversation{id: c.id}
+    Repo.all(query)
+  end
+
+  def list_conversations_by_tags(:or, tags_list) do
+    query = from t in "tags",
+            join: ct in "conversations_tags", on: t.id == ct.tag_id,
+            join: c in "conversations", on: ct.conversation_id == c.id,
+            where: t.name in ^tags_list,
+            select: %Conversation{id: c.id}
+    Repo.all(query)
+    |> Enum.uniq_by(fn %{id: id} -> id end)
+  end
+
+  def list_conversations_by_tags(:and, tags_list) do
+
+  end
+
+  def list_conversations_by_segments_and_tags(:or, segments_list, tags_list) do
+    seg_list = list_conversations_by_segments(segments_list)
+    tag_list = list_conversations_by_tags(:or, tags_list)
+    seg_list -- (seg_list -- tag_list)
+  end
+
+  def list_conversations_by_segments_and_tags(:and, segments_list, tags_list) do
+    seg_list = list_conversations_by_segments(segments_list)
+    tag_list = list_conversations_by_tags(:and, tags_list)
+    seg_list -- (seg_list -- tag_list)
   end
 end
--- test/repository/conversations_test.exs
+++ test/repository/conversations_test.exs
@@ -4,12 +4,43 @@ defmodule IntercomStats.RetrieveData.ConversationsTest do
 
   alias IntercomStats.Repository.Conversations
 
-    test "list_all_conversations/0 returns all conversations" do
-      insert(:conversation, segment: build(:segment, name: "Defacto"))
-      insert(:conversation, segment: build(:segment, name: "Defacto"))
-      insert(:conversation, segment: build(:segment, name: "Radboud"))
+    setup do
+      tag1 = insert(:tag, name: "prio1")
+      tag2 = insert(:tag, name: "prio2")
+      tag3 = insert(:tag, name: "bug")
+      tag4 = insert(:tag, name: "assistance")
+      segment1 = insert(:segment, name: "Defacto")
+      segment2 = insert(:segment, name: "Radboud")
+      insert(:conversation, segment: segment1, tags: [tag1, tag3])
+      insert(:conversation, segment: segment1, tags: [tag1, tag4])
+      insert(:conversation, segment: segment2, tags: [tag2, tag3])
+      :ok
+    end
 
+    test "list_all_conversations/0 returns all conversations" do
       returned_list = Conversations.list_all_conversations
       assert Enum.count(returned_list) == 3
+    end
+
+    test "listing conversations per segment filtered on tags/or" do
+      returned_list = Conversations.list_conversations_by_segments_and_tags(:or, ["Defacto"],["prio1", "bug"])
+      assert Enum.count(returned_list) == 2
+    end
+
+    test "listing conversations per segment filtered on tags/and" do
+      returned_list = Conversations.list_conversations_by_segments_and_tags(:and, ["Defacto"],["prio1", "bug"])
+      assert Enum.count(returned_list) == 1
+    end
+
+    test "list_conversations_by_segment/2 returns conversations filtered on segments" do
+      result_segment = Conversations.list_conversations_by_segments(["Defacto"])
+      result_segments = Conversations.list_conversations_by_segments(["Defacto", "Radboud"])
+      assert Enum.count(result_segment) == 2
+      assert Enum.count(result_segments) == 3
+    end
+
+    test "list_conversations_by_tags/2 returns conversations filtered on tags" do
+      result_tags = Conversations.list_conversations_by_tags(:or, ["prio1", "bug"])
+      assert Enum.count(result_tags) == 3
     end
 end
