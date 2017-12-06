e997a93df164a6eb2400de020c2c508ad74b36ea
Blackjaguar18 <online_richard@hotmail.com>
2017-12-06 16:39:50 +0100

    Include Conversations unit testing

--- test/intercom_stats/intercom/conversations_test.exs
+++ test/intercom_stats/intercom/conversations_test.exs
@@ -1,5 +1,18 @@
 defmodule IntercomStats.Intercom.ConversationsTest do
   use IntercomStatsWeb.ConnCase
-  import Ecto.Query
+  import IntercomStats.Factory
 
+  alias IntercomStats.Intercom.{Tags, Conversations, Conversation}
+  alias IntercomStats.Repo
+
+  test "only conversations that are usefull are saved" do
+    insert :intercom_conversation
+
+    Tags.save_from_api
+    Conversations.save_from_api
+
+    result = Repo.all(Conversation)
+    assert Enum.count(result) == 1
+    assert List.first(result).id == "3"
+  end
 end
