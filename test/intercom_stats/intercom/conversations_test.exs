3e20ea89d844fdea78bc02499f5c0aa8947b566b
Blackjaguar18 <online_richard@hotmail.com>
2017-12-06 16:46:23 +0100

    Include field test

--- test/intercom_stats/intercom/conversations_test.exs
+++ test/intercom_stats/intercom/conversations_test.exs
@@ -5,7 +5,7 @@ defmodule IntercomStats.Intercom.ConversationsTest do
   alias IntercomStats.Intercom.{Tags, Conversations, Conversation}
   alias IntercomStats.Repo
 
-  test "only conversations that are usefull are saved" do
+  test "only correct and complete conversations are stored" do
     insert :intercom_conversation
 
     Tags.save_from_api
@@ -14,5 +14,17 @@ defmodule IntercomStats.Intercom.ConversationsTest do
     result = Repo.all(Conversation)
     assert Enum.count(result) == 1
     assert List.first(result).id == "3"
+  end
+
+  test "correct values are stored for conversations" do
+    insert :intercom_conversation
+
+    Tags.save_from_api
+    Conversations.save_from_api
+
+    conversation = Repo.get(Conversation, "3")
+    assert conversation.id == "3"
+    assert conversation.company_name == "company_name_2"
+    assert conversation.closing_time == 7000
   end
 end
