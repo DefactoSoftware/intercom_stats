6df572930a42686c5a216eecb10d0cff74734b72
Blackjaguar18 <online_richard@hotmail.com>
2017-12-20 16:09:36 +0100

    Update already included events

--- lib/intercom_stats/intercom/worker.ex
+++ lib/intercom_stats/intercom/worker.ex
@@ -198,8 +198,15 @@ defmodule IntercomStats.Intercom.Worker do
   end
 
   def insert_conversation(attrs) do
-    {:ok, conversation} =
-      Repo.insert(Conversation.changeset(%Conversation{}, attrs))
+    result =
+      case Repo.get(Conversation, attrs["id"]) do
+        nil -> %Conversation{}
+        conversation -> conversation
+      end
+      |> Conversation.changeset(attrs)
+      |> Repo.insert_or_update
+
+    {:ok, conversation} = result
 
     conversation
     |> Repo.preload(:tags)
