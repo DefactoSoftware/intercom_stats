8b153fe9e9e921403250ddb984f5334fad9565b3
GlennGeelen <g.geelen@defacto.nl>
2018-01-22 14:10:09 +0100

    Add date and difference in bug prio

--- lib/intercom_stats_web/controllers/company_controller.ex
+++ lib/intercom_stats_web/controllers/company_controller.ex
@@ -1,22 +1,50 @@
 defmodule IntercomStatsWeb.CompanyController do
   use IntercomStatsWeb, :controller
+  use Timex
 
   alias IntercomStats.Repository.Conversations
 
-  def show(conn, %{"name" => name}) do
-    bug_averages = Conversations.conversation_averages_by_tag_and_company(%{
-      tag: "bug",
-      company_name: name
-    })
+  def show(conn,
+      %{"name" => name, "from_date" => from_date, "to_date" => to_date}) do
+    filter = %{
+      company_name: name,
+      from_date: from_date,
+      to_date: to_date
+    }
 
-    support_averages = Conversations.conversation_averages_by_tag_and_company(%{
-      tag: "gebruikersondersteuning",
-      company_name: name
-    })
+    prio1_averages =
+      %{tag: "prio 1"}
+      |> Map.merge(filter)
+      |> Conversations.conversation_averages_by_tag_and_company()
+
+    prio2_averages =
+      %{tag: "prio 2"}
+      |> Map.merge(filter)
+      |> Conversations.conversation_averages_by_tag_and_company()
+
+    prio3_averages =
+      %{tag: "prio 3"}
+      |> Map.merge(filter)
+      |> Conversations.conversation_averages_by_tag_and_company()
+
+    prio4_averages =
+      %{tag: "prio 4"}
+      |> Map.merge(filter)
+      |> Conversations.conversation_averages_by_tag_and_company()
+
+    support_averages =
+      %{tag: "gebruikersondersteuning"}
+      |> Map.merge(filter)
+      |> Conversations.conversation_averages_by_tag_and_company()
 
     model = %{
       company_name: name,
-      bug_averages: bug_averages,
+      from_date: from_date,
+      to_date: to_date,
+      prio1_averages: prio1_averages,
+      prio2_averages: prio2_averages,
+      prio3_averages: prio3_averages,
+      prio4_averages: prio4_averages,
       support_averages: support_averages
     }
 
