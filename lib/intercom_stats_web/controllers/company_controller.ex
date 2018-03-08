a1223ad5c9818188878bc14a6caa4a79146e287d
GlennGeelen <g.geelen@defacto.nl>
2018-03-08 15:15:46 +0100

    Fix test

--- lib/intercom_stats_web/controllers/company_controller.ex
+++ lib/intercom_stats_web/controllers/company_controller.ex
@@ -6,12 +6,37 @@ defmodule IntercomStatsWeb.CompanyController do
 
   def show(conn,
       %{"name" => name, "from_date" => from_date, "to_date" => to_date}) do
+
     filter = %{
       company_name: name,
-      from_date: from_date,
-      to_date: to_date
+      from_date: default_from_date(from_date),
+      to_date: default_to_date(to_date)
     }
 
+    conn
+    |> assign(:model, create_model(filter))
+    |> render("show.html")
+  end
+
+  def show(conn, %{"name" => name}) do
+    filter = %{
+      company_name: name,
+      from_date: default_from_date(nil),
+      to_date: default_to_date(nil)
+    }
+
+    conn
+    |> assign(:model, create_model(filter))
+    |> render("show.html")
+  end
+
+  defp default_from_date(nil), do: "2017-07-01"
+  defp default_from_date(date), do: date
+
+  defp default_to_date(nil), do: Date.to_string(Date.utc_today())
+  defp default_to_date(date), do: date
+
+  defp create_model(filter) do
     prio1_averages =
       %{tag: "prio 1"}
       |> Map.merge(filter)
@@ -38,18 +63,14 @@ defmodule IntercomStatsWeb.CompanyController do
       |> Conversations.conversation_averages_by_tag_and_company()
 
     model = %{
-      company_name: name,
-      from_date: from_date,
-      to_date: to_date,
+      company_name: filter.company_name,
+      from_date: filter.from_date,
+      to_date: filter.to_date,
       prio1_averages: prio1_averages,
       prio2_averages: prio2_averages,
       prio3_averages: prio3_averages,
       prio4_averages: prio4_averages,
       support_averages: support_averages
     }
-
-    conn
-    |> assign(:model, model)
-    |> render("show.html")
   end
 end
