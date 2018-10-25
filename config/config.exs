c1dd01e6268102f401ab27572d5ae6b4fe50019b
GlennGeelen <g.geelen@defacto.nl>
2018-10-25 12:17:50 +0200

    move sentry config and add necessary responders for coherence

--- config/config.exs
+++ config/config.exs
@@ -14,17 +14,29 @@ config :intercom_stats, IntercomStatsWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "7nqkHxuBDAyqv7PFekn/owiY5/4cEugOqzFOToQL10bybcsPjs9jEljan3tOS20H",
   render_errors: [view: IntercomStatsWeb.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: IntercomStats.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: IntercomStats.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :sentry,
+  dsn:
+    "https://#{System.get_env("SENTRY_PUBLIC_KEY")}:#{System.get_env("SENTRY_SECRET_KEY")}@#{
+      System.get_env("SENTRY_APP")
+    }.getsentry.com/1",
+  environment_name: :prod,
+  enable_source_code_context: true,
+  root_source_code_path: File.cwd!(),
+  tags: %{
+    env: "production"
+  },
+  included_environments: [:prod]
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
 
 # %% Coherence Configuration %%   Don't remove this line
 config :coherence,
@@ -36,4 +48,5 @@ config :coherence,
   messages_backend: IntercomStatsWeb.Coherence.Messages,
   logged_out_url: "/",
   opts: [:authenticatable]
+
 # %% End Coherence Configuration %%
