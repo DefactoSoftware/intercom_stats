1c270381e23802614b1bac06289c7db6022fbd31
Albert Thie <a.thie@defacto.nl>
2019-05-23 15:44:28 +0200

    Get tag list from system value

--- lib/intercom_stats_web/controllers/company_controller.ex
+++ lib/intercom_stats_web/controllers/company_controller.ex
@@ -3,7 +3,7 @@ defmodule IntercomStatsWeb.CompanyController do
   use Timex
 
   alias IntercomStats.Repository.Conversations
-
+  @taglist System.get_env("TAGS")
   def show(
         conn,
         %{"name" => name, "from_date" => from_date, "to_date" => to_date}
@@ -15,7 +15,8 @@ defmodule IntercomStatsWeb.CompanyController do
     }
 
     conn
-    |> assign(:model, create_model(filter))
+    |> assign(:total_model, create_total_model(filter))
+    |> assign(:tag_models, create_tag_models(filter))
     |> render("show.html")
   end
 
@@ -27,7 +28,8 @@ defmodule IntercomStatsWeb.CompanyController do
     }
 
     conn
-    |> assign(:model, create_model(filter))
+    |> assign(:total_model, create_total_model(filter))
+    |> assign(:tag_models, create_tag_models(filter))
     |> render("show.html")
   end
 
@@ -36,20 +38,32 @@ defmodule IntercomStatsWeb.CompanyController do
 
   defp default_to_date(nil), do: Date.to_string(Date.utc_today())
   defp default_to_date(date), do: date
-  defp add_to_model(key, value, model), do: put(model, key, value)
-  taglist = ["prio 1", "prio 2", "prio 3", "prio 4", "prio 5", "gebruikersondersteuning"]
 
-  defp create_model(filter) do
-    untagged_averages =
-      %{tag: nil}
-      |> Map.merge(filter)
-      |> Conversations.conversation_stats_by_tag_and_company()
+  defp add_to_model(key, value, tagmodel) do
+    Map.put(tagmodel, key, value)
+  end
+
+  defp create_tag_models(filter) do
+    @taglist
+    |> Enum.map(fn tag ->
+      %{tag: tag, stats: generate_stats(filter, tag)}
+    end)
+  end
+
+  defp generate_stats(filter, tag) do
+    %{tag: tag}
+    |> Map.merge(filter)
+    |> Conversations.conversation_stats_by_tag_and_company()
+  end
+
+  defp create_total_model(filter) do
+    untagged_averages = generate_stats(filter, nil)
 
     total_averages = Conversations.conversation_stats_by_tag_and_company(filter)
 
     message_number = Conversations.conversation_number_by_company(filter)
 
-    model = %{
+    %{
       company_name: filter.company_name,
       from_date: filter.from_date,
       to_date: filter.to_date,
@@ -57,16 +71,5 @@ defmodule IntercomStatsWeb.CompanyController do
       untagged_averages: untagged_averages,
       message_number: message_number
     }
-
-    Enum.each(taglist, fn x ->
-      averages =
-        %{tag: x}
-        |> Map.merge(filter)
-        |> Conversations.conversation_stats_by_tag_and_company()
-
-      String.trim(x)
-      |> String.to_atom()
-      |> add_to_model(averages, model)
-    end)
   end
 end
